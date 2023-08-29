f50a76fd8ac970b10b9593e6f8b4473d1240dd30
JESUS-Baby <98994174+JESUS-Baby@users.noreply.github.com>
2023-08-29 13:52:01 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,24 +2,20 @@
 
 int main(void)
 {
-    /* Declare a variable and initialize it with the first member of 
-       the range [a - z] 
-    */
-
+    /* Declare a variable and initialize it with the first member of
+    the range [a - z] */
     char alphabet = 'a';
-
     /* check to see if the current value of your variable is less than
          or the same as the last member (z) of the given range. While 
          the value is less, go ahead and print the value
       */
-
-while (alphabet <= 'z')
+    while (alphabet <= 'z')
 {
-        /* print the value of the variable */
-    putchar(alphabet);
+/* print the value of the variable */
+putchar(alphabet);
 
     /* increment the variable (ie: a becomes b)  */
-    alphabet++;
+alphabet++;
 }
 
 putchar('\n');
